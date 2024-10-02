1a3b24fd09659520e29b5efc30a55e71f26d7290
XxAndressaxX <dressaoficial123@gmail.com>
2024-10-02 10:38:18 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/XxAndressaxX/Coleta-de-dados/refs/heads/main/matematica.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
